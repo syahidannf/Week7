cbd4c44dae5ee27af483aef349752686d33d2e95
syahidannf <118803307+syahidannf@users.noreply.github.com>
2023-02-18 04:10:41 +0700

    hw7

---
+++ soal1.js
@@ -0,0 +1,9 @@
+const calc = require("./calc.js");
+
+console.log("Luas Persegi", calc.luasPersegi(20, 4));
+
+console.log("Keliling Persegi", calc.KelPersegi(40, 5));
+
+console.log("Luas Persegi Panjang", calc.luasPP(60, 6));
+
+console.log("Keliling Persegi Panjang", calc.KelPP(80, 7));
